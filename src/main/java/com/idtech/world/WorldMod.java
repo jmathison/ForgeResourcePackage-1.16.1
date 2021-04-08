fd2e9ea0a58eb33ae9020dcdad5deb418df79133
charlotte <charlotteec>
2021-04-08 15:43:52 -0700

    biomes update

--- src/main/java/com/idtech/world/WorldMod.java
+++ src/main/java/com/idtech/world/WorldMod.java
@@ -1,4 +1,20 @@
 package com.idtech.world;
 
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeManager;
+import net.minecraftforge.event.RegistryEvent;
+
 public class WorldMod {
+
+    public static void registerBiomes(final RegistryEvent.Register<Biome> event){
+        event.getRegistry().register(ObsidianHillsBiome.INSTANCE);
+
+    }
+    public static void setupBiomes(){
+        BiomeDictionary.addTypes(ObsidianHillsBiome.INSTANCE, BiomeDictionary.Type.DRY, BiomeDictionary.Type.OVERWORLD);
+        BiomeManager.addBiome(BiomeManager.BiomeType.DESERT, new BiomeManager.BiomeEntry(ObsidianHillsBiome.INSTANCE, 500));
+        BiomeManager.addSpawnBiome(ObsidianHillsBiome.INSTANCE);
+
+    }
 }
