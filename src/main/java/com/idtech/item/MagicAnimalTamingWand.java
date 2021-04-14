2f977e08681e3c4e81da599d7a1ab04c5b497df7
charlotte <charlotteec>
2021-04-14 15:57:43 -0700

    animal taming wand

--- src/main/java/com/idtech/item/MagicAnimalTamingWand.java
+++ src/main/java/com/idtech/item/MagicAnimalTamingWand.java
@@ -1,4 +1,39 @@
 package com.idtech.item;
 
-public class MagicAnimalTamingWand {
+import com.idtech.BaseMod;
+import com.idtech.Utils;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+public class MagicAnimalTamingWand extends Item{
+
+    //properties object
+    public static Item.Properties properties = new Item.Properties().group(ItemGroup.MISC);
+    //static instance for registration
+    public static Item INSTANCE = new MagicAnimalTamingWand(properties).setRegistryName(BaseMod.MODID, "magicanimal");
+
+    //constructor
+    public MagicAnimalTamingWand(Item.Properties properties){
+        super(properties);
+
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+
+        Entity entity = Utils.getEntityAtCursor(playerIn, 100);
+
+        playerIn.startRiding(entity);
+
+        return super.onItemRightClick(worldIn, playerIn, handIn);
+
+
+    }
 }
