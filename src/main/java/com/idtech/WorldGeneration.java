11ee24db641aeb77a14bc13dd8efd1086e1cbc9c
afroraydude <3768904+afroraydude@users.noreply.github.com>
2021-06-17 15:25:49 -0400

    Ore generation

---
+++ src/main/java/com/idtech/WorldGeneration.java
@@ -0,0 +1,70 @@
+package com.idtech;
+import com.google.common.collect.Lists;
+import com.idtech.block.BlockMod;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.feature.template.BlockMatchRuleTest;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.placement.IPlacementConfig;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * World/Ore generation
+ * @author afroraydude (Ray "Stitch" Thomas)
+ */
+public class WorldGeneration {
+
+    private static Stream<Biome> biomes;
+
+    public static void applyBiomeFeatures() {
+        biomes = getDifferentBiomes();
+
+
+        // only in certain biomes
+
+        // this commented out example uses emerald ore as its template
+        // generateFeatures(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.EMERALD_ORE.withConfiguration(new ReplaceBlockConfig(Blocks.STONE.getDefaultState(), BlockMod.GEL_ORE_BLOCK.getDefaultState())).withPlacement(Placement.EMERALD_ORE.configure(IPlacementConfig.NO_PLACEMENT_CONFIG)), Biomes.BADLANDS);
+
+        // in all biomes
+
+        // this commented out example uses emerald ore as its template
+        //generateFeaturesInAllBiomes(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.EMERALD_ORE.withConfiguration(new ReplaceBlockConfig(Blocks.STONE.getDefaultState(), BlockMod.GEL_ORE_BLOCK.getDefaultState())).withPlacement(Placement.EMERALD_ORE.configure(IPlacementConfig.NO_PLACEMENT_CONFIG)));
+
+        // this example does not
+        // vein size 10, count per generation 20, max y level 64
+        // modify above numbers to suit your needs
+        // this will only work on newly generated chunks, so best way to test is to make a new world or to teleport FAR FAR AWAY in your current world
+        generateFeaturesInAllBiomes(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockMod.GEL_ORE_BLOCK.getDefaultState(), 10)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(20, 0, 0, 64))));
+    }
+
+    protected static void generateFeatures(GenerationStage.Decoration stageIn, ConfiguredFeature<?, ?> featureIn, Biome... biomesIn) {
+        for(Biome biome : biomesIn) biome.addFeature(stageIn, featureIn);
+    }
+
+    protected static void generateFeaturesExcept(GenerationStage.Decoration stageIn, ConfiguredFeature<?, ?> featureIn, Biome... biomesIn) {
+        List<Biome> list = Lists.newArrayList(biomesIn);
+        biomes.filter(biome -> !list.contains(biome)).forEach(biome -> biome.addFeature(stageIn, featureIn));
+    }
+
+    protected static void generateFeaturesInAllBiomes(GenerationStage.Decoration stageIn, ConfiguredFeature<?, ?> featureIn) {
+        biomes.forEach(biome -> biome.addFeature(stageIn, featureIn));
+    }
+
+    private static Stream<Biome> getDifferentBiomes() {
+        return ForgeRegistries.BIOMES.getValues().stream().filter(biome -> !biome.getRegistryName().getNamespace().equals(BaseMod.MODID));
+    }
+}
