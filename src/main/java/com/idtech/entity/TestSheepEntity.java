b7dc977c1807d356574c03ec607454f549225587
afroraydude <3768904+afroraydude@users.noreply.github.com>
2021-07-02 17:18:36 -0400

    Possibly better custom sheep

--- src/main/java/com/idtech/entity/TestSheepEntity.java
+++ src/main/java/com/idtech/entity/TestSheepEntity.java
@@ -8,13 +8,21 @@ import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierManager;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
 public class TestSheepEntity extends SheepEntity {
+
+    private int sheepTimer;
+    private EatGrassGoal eatGrassGoal;
+
     public static EntityType<TestSheepEntity> TYPE = (EntityType<TestSheepEntity>)
             EntityType.Builder.create(TestSheepEntity::new, EntityClassification.CREATURE).build("sheepo").setRegistryName(BaseMod.MODID, "sheepo");
     //EGG
@@ -26,6 +34,22 @@ public class TestSheepEntity extends SheepEntity {
 
     public static AttributeModifierMap.MutableAttribute func_234225_eI_() {
         return MobEntity.func_233666_p_().func_233815_a_(Attributes.field_233818_a_, 8.0D).func_233815_a_(Attributes.field_233821_d_, (double)0.5F);
+    }
+
+    protected void registerGoals() {
+        super.registerGoals();
+    }
+
+    protected void updateAITasks() {
+        super.updateAITasks();
+    }
+
+    /**
+     * Called frequently so the entity can update its state every tick as required. For example, zombies and skeletons
+     * use this to react to sunlight and start to burn.
+     */
+    public void livingTick() {
+        super.livingTick();
     }
 
 }
